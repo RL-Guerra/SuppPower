2b685623bdc91ebd3e7dc193880eaf0a03a2daca
Raphael Guerra de Lima <154474063+RL-Guerra@users.noreply.github.com>
2025-07-17 13:59:24 -0300

    Updated products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -212,7 +212,7 @@ export const products: Product[] = [
     name: "Sundown Vitamina E 1001ui 30 cápsulas",
     price: 94.50,
     originalPrice: 105.00,
-    image: "https://tse3.mm.bing.net/th/id/OIP.bZJEhTXP00Y9oR_o6anjBwHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
+    image: "https://product-data.raiadrogasil.io/images/10878119.webp",
     category: "Vitaminas",
     description: "Vitamina E com potente ação antioxidante, protege células contra radicais livres, promove saúde da pele, fortalece o sistema nervoso e contribui para o anti-envelhecimento natural.",
     benefits: ["Antioxidante", "Protege células", "Saúde da pele", "Sistema nervoso"],
