6971ce676eb78fe6a48ca031f15bf62d98c6a443
Raphael Guerra de Lima <154474063+RL-Guerra@users.noreply.github.com>
2025-07-18 12:20:34 -0300

    Updated ProductSlider.tsx

--- src/components/ProductSlider.tsx
+++ src/components/ProductSlider.tsx
@@ -12,7 +12,7 @@ export const ProductSlider: React.FC = () => {
       setCurrentIndex((prevIndex) => 
         prevIndex === featuredProducts.length - 1 ? 0 : prevIndex + 1
       );
-    }, 5000); // Muda a cada 3 segundos
+    }, 4000); // Muda a cada 3 segundos
 
     return () => clearInterval(interval);
   }, [featuredProducts.length]);
